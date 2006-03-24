1c828140cf5a012e3b3d6dd3eec4f08541b5ea80
Adam Kennedy <adamk@cpan.org>
2006-03-24 18:36:52 +0000

    Import of ADAMK/Lingua-EN-VarCon-0.01 from CPAN.
    gitpan-cpan-distribution: Lingua-EN-VarCon
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ADAMK/Lingua-EN-VarCon-0.01.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

---
+++ inc/Module/Install/Share.pm
@@ -0,0 +1,32 @@
+#line 1
+package Module::Install::Share;
+
+BEGIN {
+	$VERSION = '0.62';
+	@ISA     = qw{Module::Install::Base};
+}
+
+use strict;
+use Module::Install::Base;
+
+sub install_share {
+    my ($self, $dir) = @_;
+
+    if ( ! defined $dir ) {
+        die "Cannot find the 'share' directory" unless -d 'share';
+        $dir = 'share';
+    }
+
+    $self->postamble(<<"END_MAKEFILE");
+config ::
+\t\$(NOECHO) \$(MOD_INSTALL) \\
+\t\t\"$dir\" \$(INST_AUTODIR)
+
+END_MAKEFILE
+}
+
+1;
+
+__END__
+
+#line 90
